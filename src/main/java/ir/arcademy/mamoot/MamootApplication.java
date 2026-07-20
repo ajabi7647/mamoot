09fca3d140c7e2226b5804465a4abf75415b8d9f
Afshar <abolfazlafshar52@yahoo.com>
2026-07-20 05:03:12 -0800

    Initial commit

---
+++ src/main/java/ir/arcademy/mamoot/MamootApplication.java
@@ -0,0 +1,13 @@
+package ir.arcademy.mamoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/arcademy/mamoot/MamootApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.arcademy.mamoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
