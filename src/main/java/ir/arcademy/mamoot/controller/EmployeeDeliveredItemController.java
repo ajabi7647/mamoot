c34e2a598ce040901f06d96925af3826b58d96fb
Administrator <abolfazlafshar52@yahoo.com>
2026-08-17 02:49:23 -0700

    deliver and reciver entity  deploy

---
+++ src/main/java/ir/arcademy/mamoot/controller/EmployeeDeliveredItemController.java
@@ -0,0 +1,17 @@
+package ir.arcademy.mamoot.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Configuration
+@RequestMapping("api/v1/delivered")
+public class EmployeeDeliveredItemController {
+
+    //GET
+    @GetMapping
+    public ResponseEntity<<>>
+
+
+}
--- src/main/java/ir/arcademy/mamoot/mapper/EmployeeDeliveredItemMapper.java
+++ src/main/java/ir/arcademy/mamoot/mapper/EmployeeDeliveredItemMapper.java
@@ -8,17 +8,22 @@ import java.util.UUID;
 public class EmployeeDeliveredItemMapper {
 
     public EmployeeDeliveredItemDto toDto(EmployeeDeliveredItem entity) {
-         //تبدیل کدهای ساده
+         //تبدیل فیلدهای ساده
         EmployeeDeliveredItemDto dto = new EmployeeDeliveredItemDto();
         dto.setQuantity(entity.getQuantity());
         dto.setAssetCode(entity.getAssetCode());
         dto.setSerial(entity.getSerial());
         dto.setUnit(entity.getUnit());
         dto.setDescription(entity.getDescription());
+        //تبدیل فیلدهای رابطه ای
         dto.setReceiverId(entity.getReceiver().getId());
         dto.setDelivererId(entity.getDeliverer().getId());
-        dto.setAssetTypeId(entity.getAssetType().getId());
-        dto.setConsumableItemId(entity.getConsumableItem().getId());
+        if (entity.getAssetType() != null) {
+            dto.setAssetTypeId(entity.getAssetType().getId());
+        }
+        if (entity.getConsumableItem() != null) {
+            dto.setConsumableItemId(entity.getConsumableItem().getId());
+        }
         return dto;
 
          }
